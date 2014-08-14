e2ee4833c81f48a0c52209ff77b280a152e7ad4c
Flysociety <Flysociety@Flysocietys-MacBook-Pro.local>
2014-08-14 16:47:37 -0500

    FIZZ BUZZ practice

---
+++ FizzBuzz.php
@@ -0,0 +1,34 @@
+<?php
+
+/* $number = 1;
+
+while ($number<100){
+		if($number % 3 == 0){
+			echo "FIZZle\n";
+		}
+
+		elseif($number % 5 == 0){
+			echo "Buzz-izzle\n";
+		}
+		else {
+			echo "$number\n";
+			}
+
+			$number++;
+
+	}
+*/
+
+	for ($number=0; $number <= 100; $number++){
+		
+		if ($number % 3 == 0){
+			echo "FIZZle\n";
+		}
+		elseif ($number % 5 == 0){
+			echo "Buzz-izzle\n";
+		}
+		else {
+			echo "$number\n";
+		}
+		}
+?>
