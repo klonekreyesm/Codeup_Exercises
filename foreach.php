977c288aa3b36f459ce72b983430e7270434cc7a
Flysociety <Flysociety@Flysocietys-MacBook-Pro.local>
2014-08-15 10:24:36 -0500

    exercise 2 output with array foreach loop

--- foreach.php
+++ foreach.php
@@ -21,20 +21,53 @@
 
 
 
-$data = array(42, 'bacon', (6 * 3), 'The Big Bang Theory', 98.6);
+//$data = array(42, 'bacon', (6 * 3), 'The Big Bang Theory', 98.6); //sets array of what we want to run in loop
 
-foreach ($data as $datum) {
+// foreach ($data as $datum) {										  // foreach loop sets the array to represent an element in the array
 
-    if (is_numeric($datum)) {
-		echo "{$datum} is a number\n";
-} 
-	else if (is_string($datum)) {
-        echo "{$datum} is a string\n";
-    }
-}
+//     if (is_numeric($datum)) {									// if the variable is numeric echo out this is a number
+// 		echo "{$datum} is a number\n";	
+// } 
+// 	else if (is_string($datum)) {								// else if the variable is a string echo this is a string
+//         echo "{$datum} is a string\n";
+//     }
+// }
 
 
 
+
+$things = array('Sgt.Pepper', 11, null, array (1,2,3), 3.14, "12+7", false, (string)11);
+
+foreach($things as $data){
+
+	if(is_integer($data)) {							
+			echo "$data is a number.\n";
+			usleep(500000);
+		}
+
+	else if (is_float($data)){
+			echo "$data is a float. \n";
+			usleep(500000);
+		}
+	else if (is_bool($data)){
+			echo "$data is boolean.\n";
+			usleep(500000);
+		}
+	else if (is_array($data)){
+			echo "$data is an array.\n";
+			usleep(500000);
+		}
+	else if (is_null($data)){
+			echo null ."\n";
+			usleep(500000);
+		}
+	else if(is_string($data)){
+			echo "$data is a string.\n";
+			usleep(500000);
+		}
+
+	
+	}
 
 
 
