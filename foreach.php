9027a42d9df3fb22b4833ac13750c46d58d0202b
Flysociety <Flysociety@Flysocietys-MacBook-Pro.local>
2014-08-15 10:47:55 -0500

    EXERCISE foreach with identifying variables

--- foreach.php
+++ foreach.php
@@ -36,38 +36,77 @@
 
 
 
-$things = array('Sgt.Pepper', 11, null, array (1,2,3), 3.14, "12+7", false, (string)11);
+// $things = array('Sgt.Pepper', 11, null, array (1,2,3), 3.14, "12+7", false, (string)11);
+
+// foreach($things as $data){
+
+// 	if(is_integer($data)) {							
+// 			echo "$data is a number.\n";
+// 			usleep(500000);
+// 		}
+
+// 	else if (is_float($data)){
+// 			echo "$data is a float. \n";
+// 			usleep(500000);
+// 		}
+// 	else if (is_bool($data)){
+// 			echo "$data is boolean.\n";
+// 			usleep(500000);
+// 		}
+// 	else if (is_array($data)){
+// 			echo "$data is an array.\n";
+// 			usleep(500000);
+// 		}
+// 	else if (is_null($data)){
+// 			echo null ."\n";
+// 			usleep(500000);
+// 		}
+// 	else if(is_string($data)){
+// 			echo "$data is a string.\n";
+// 			usleep(500000);
+// 		}
+
+	
+// 	}
+
+$things = array('Sgt.Pepper', 11, null, array(1,2,3), 3.14, "12+7", false, (string)11);//set array to variable
 
 foreach($things as $data){
 
-	if(is_integer($data)) {							
-			echo "$data is a number.\n";
-			usleep(500000);
-		}
+	if(is_scalar($data)){
 
-	else if (is_float($data)){
-			echo "$data is a float. \n";
-			usleep(500000);
-		}
-	else if (is_bool($data)){
-			echo "$data is boolean.\n";
-			usleep(500000);
-		}
-	else if (is_array($data)){
-			echo "$data is an array.\n";
-			usleep(500000);
-		}
-	else if (is_null($data)){
-			echo null ."\n";
-			usleep(500000);
-		}
-	else if(is_string($data)){
-			echo "$data is a string.\n";
-			usleep(500000);
-		}
-
-	
+		if(is_integer($data)) {							
+				echo $data."\n";
+				usleep(500000);
+			}
+		else if (is_float($data)){
+				echo $data."\n";
+				usleep(500000);
+			}
+		else if (is_bool($data)){
+				echo $data."\n";
+				usleep(500000);
+			}
+		else if (is_array($data)){
+				echo print_r."\n";
+				usleep(500000);
+			}
+		else if (is_null($data)){
+				echo null ."\n";
+				usleep(500000);
+			}
+		else if(is_string($data)){
+				echo $data ."\n";
+				usleep(500000);
+			
 	}
+}
+	else {
+		echo null ."\n";
+		usleep(50000);
+	}
+}
+	
 
 
 
