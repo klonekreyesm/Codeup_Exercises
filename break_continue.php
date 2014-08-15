b16bba3ed2dbd066d03f4b45d1bf015832ca9d79
Flysociety <Flysociety@Flysocietys-MacBook-Pro.local>
2014-08-15 15:45:46 -0500

    exercise create for loop that counts from 1 to 100 and breaks at 10

--- break_continue.php
+++ break_continue.php
@@ -1,12 +1,12 @@
 <?php
 
 
-for ($i = 1; $i<=100; $i++){							//for loop that counts to 100
-
-	if ($i % 2 == 0){									//check to see if it is divisible by 2 that dictats an even number
-		echo $i. "\n";									//print even numbers
+for ($i = 1; $i<=100; $i++){						//for loop that counts to 100
+echo $i. "\n";	
+	if ($i == 10){									//break at 10
+	 break;											
+													
 	}
-		continue;
 }
 
 
