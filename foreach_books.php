9b49d0c89e5db778a09bc8128b0183e76e33eeed
Flysociety <Flysociety@Flysocietys-MacBook-Pro.local>
2014-08-15 13:53:46 -0500

    last changes to foreach exercises. this exercise
    Construct a loop that iterates through each book and then each book's keys and values. Have it output the book's title, then list the key value pairs for the data about each book.
    
    Update your loop to only show books that were written after 1950.

--- foreach_books.php
+++ foreach_books.php
@@ -1,7 +1,8 @@
 <?php
 
-$books = array(
-    'The Hobbit' => array(
+$books = array(                                                 //an array within an array that holds data
+
+    'The Hobbit' => array(                                      //in this case, each $books has an array with KEYS of titles and values of data
         'published' => 1937,
         'author' => 'J. R. R. Tolkien',
         'pages' => 310
@@ -16,16 +17,48 @@ $books = array(
         'author' => 'J. D. Salinger',
         'pages' => 220
     ),
-    'A Tale of Two Cities' => array(
+    'A Tale of Two Cities' =>array(
         'published' => 1859,
         'author' => 'Charles Dickens',
         'pages' => 544
     )
 );
 
-foreach($books as $key => $value){
-		echo $key."\n";
+ //if ($book_data)
+        foreach ($books as $title => $book_data){
+
+           if($book_data['published']> 1950){                       //if statement to identify if the published date is greater than 1950
+
+             echo "--------------------------\n";                   //executes IF the statement above is TRUE
+             echo "$title\n";
+             echo "published in ". $book_data['published']."\n";    // echos the data pulled from the second array
+             echo "written by ". $book_data['author']."\n";
+             echo $book_data['pages']." pages \n";
+
+
+        //foreach ($book_data as $key => $value){
+	    //	echo "$key $value \n";
+		
+	}
 }
+
+
+	//echo $book_data;
+
+//	foreach($title as $book_data){
+	//echo $book_data;
+//}
+
+
+	//foreach($title as $book_data => $value){
+		//echo $value . "\n";
+
+	
+
+
+
+
+
 
 
 
