7669bef21de53df2b24270e1c4ba61f38688c354
Flysociety <Flysociety@Flysocietys-MacBook-Pro.local>
2014-08-18 16:07:34 -0500

    arithmetic functions

--- arithmetic.php
+++ arithmetic.php
@@ -1,37 +1,69 @@
 <?php
 
+        
+        function add($a,$b){	
+			if (is_numeric($a) && is_numeric($b)) {					//define addition function
+			echo $a + $b. PHP_EOL;
+			usleep (500000);
+			}else {
+        	echo "ERROR: Both arguments must be numbers".PHP_EOL;
+    		}
+		}
+		function subtract($a,$b){									//define subtraction function
+			if (is_numeric($a) && is_numeric($b)){									
+			echo $a - $b. PHP_EOL;
+			usleep (500000);
+			}else {
+        	echo "ERROR: Both arguments must be numbers".PHP_EOL;
+    		}
+		}
+		function multiply($a,$b){									//define multiplication function
+			if(is_numeric($a) && is_numeric($b)){
+			echo $a * $b. PHP_EOL;
+			usleep (500000);
+			}else{
+			echo "ERROR: Both arguments must be numbers".PHP_EOL;
+			}
+		}
+		function divide ($a,$b){									//define division function
+			if(is_numeric($a) && is_numeric($b)){
+				if($b == 0){
+					echo "ERROR: Not divisible by 0.".PHP_EOL;
+				}else{
+					echo $a / $b. PHP_EOL;
+					usleep (500000);
+				}
+			}else{
+			echo "ERROR: Both arguments must be numbers".PHP_EOL;
+			}
+		}
+	 	function remainder($a,$b){									//define modulus function
+	 		if (is_numeric($a) && is_numeric($b)){
+	 			if($b == 0){
+					echo "ERROR: Not divisible by 0";				
+				}else{
+					echo $a % $b. PHP_EOL;
+					usleep (500000);
+				}
+			}else{
+			echo "ERROR: Both arguments must be numbers".PHP_EOL;
+			}
 
-	function add($a,$b){						//define addition function
-		echo $a + $b. PHP_EOL;
-		usleep (500000);
-	}
-	function subtract($a,$b){					//define subtraction
-		echo $a - $b. PHP_EOL;
-		usleep (500000);
-	}
-	function multiply($a,$b){					//define multiplication
-		echo $a * $b. PHP_EOL;
-		usleep (500000);
-	}
-	function divide ($a,$b){					//define division
-		echo $a / $b. PHP_EOL;
-		usleep (500000);
-	}
-	 function remainder($a,$b){					//define modulus
-		echo $a % $b. PHP_EOL;
-		usleep (500000);
-	}
+		}
+
+
 
 
 	add(2,3);									//test functions
-
 	subtract(3,1);
-
 	multiply(2,3);
-
 	divide(2,3);
-
 	remainder(10,3);
+	add('Low',0);
+	subtract ('Low','test');
+	remainder (12,'test');
+	divide(12,0);
+	remainder (3,0);
 
 
 	
