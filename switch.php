25d603ebd2f39688660ebf7d9cb71317eb001116
Flysociety <Flysociety@Flysocietys-MacBook-Pro.local>
2014-09-02 17:05:16 -0500

    comparison of switch and if/else statement, same equation

--- switch.php
+++ switch.php
@@ -1,50 +1,57 @@
 <?php
-
-date_default_timezone_set('America/Chicago');		//set default timezome
-$day_of_week = date('N');							//Get Day of Week as number	 			
- 													//1 (for Monday) through 7 (for Sunday)
+//set default timezome
+date_default_timezone_set('America/Chicago');		
+//Get Day of Week as number	 1 (for Monday) through 7 (for Sunday)
+$day_of_week = date('N');										
+ 													
 	switch($day_of_week) {
 	    case 1:
-	         echo "Monday \n";
+	         echo "Monday".PHP_EOL;
 	     	break;
 	    case 2:
-	         echo "Tuesday \n";
+	         echo "Tuesday".PHP_EOL;
 	     	break;
 	    case 3;
-	     	echo "Wednesday \n";
+	     	echo "Wednesday".PHP_EOL;
 	     	break;
 	    case 4:
-	     	echo "Thursday \n";
+	     	echo "Thursday".PHP_EOL;
 	     	break;
 	    case 5:
-	     	echo "Friday \n";
+	     	echo "Friday".PHP_EOL;
 	     	break;
 	    case 6:
-	     	echo "Saturday \n";
+	     	echo "Saturday".PHP_EOL;
 	     	break;
 	    case 7:
-	     	echo "Sunday \n";
+	     	echo "Sunday".PHP_EOL;
 	     	break;
 	}
 
 
-
- $day_of_week = date('N');							//same equation as above in if else 
+//same equation as above in if else 
+ $day_of_week = date('N');							
  
 if ($day_of_week == 1) {
-		echo "Monday\n";
-	}elseif($day_of_week == 2){
- 		echo "Tuesday\n";
-	}elseif($day_of_week == 3){
- 		echo "Wednesday\n";
-	}elseif($day_of_week == 4){
- 		echo "Thursday\n";
-	}elseif($day_of_week == 5){
- 		echo "Friday\n";
-	}elseif($day_of_week == 6){
- 		echo "Saturday\n";
-	}elseif($day_of_week == 7){
- 		echo "Sunday\n";
+		echo "Monday".PHP_EOL;
+	}
+	elseif($day_of_week == 2){
+ 		echo "Tuesday".PHP_EOL;
+	}
+	elseif($day_of_week == 3){
+ 		echo "Wednesday".PHP_EOL;
+	}
+	elseif($day_of_week == 4){
+ 		echo "Thursday".PHP_EOL;
+	}
+	elseif($day_of_week == 5){
+ 		echo "Friday".PHP_EOL;
+	}
+	elseif($day_of_week == 6){
+ 		echo "Saturday".PHP_EOL;
+	}
+	elseif($day_of_week == 7){
+ 		echo "Sunday".PHP_EOL;
  	}
 
 ?>
