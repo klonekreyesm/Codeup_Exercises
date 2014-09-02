e76474f33d463f7b3e31a40f10d04cdd4bc086be
Flysociety <Flysociety@Flysocietys-MacBook-Pro.local>
2014-09-02 16:33:07 -0500

    formatting applied

--- while.php
+++ while.php
@@ -1,29 +1,14 @@
 <?php
+//Create a variable $test equal to 5.
+//Create a while loop that checks $test <= 15 and increment $test by 1 inside the loop. 
+//Before incrementing $test, output each iteration (echo $test with newline) and display results.
+	$test = 5;
 
-
-
-
-$test = 5;
-
-while ($test <= 15)
-
-{
-
-	echo $test . "\n";
-	$test++ ;
+	while ($test <= 15){
+		echo $test . "\n";
+		$test++ ;
 		usleep(50000);
-
-}
-
-
-
-
-
-
-
-
-
-
+	}
 
 
 ?>
