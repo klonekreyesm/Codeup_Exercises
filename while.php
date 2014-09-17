0a62170f971b817df63f5ab1f0b02b8d5d53869d
Flysociety <Flysociety@Flysocietys-MacBook-Pro.local>
2014-09-17 18:25:13 -0500

    Formatting.

--- while.php
+++ while.php
@@ -1,14 +1,14 @@
 <?php
-//Create a variable $test equal to 5.
-//Create a while loop that checks $test <= 15 and increment $test by 1 inside the loop. 
-//Before incrementing $test, output each iteration (echo $test with newline) and display results.
-	$test = 5;
-
-	while ($test <= 15){
-		echo $test . PHP_EOL;
-		$test++;
-		usleep(50000);
-	}
+    
+     //Create a variable $test equal to 5.
+    $test = 5;
+    //Create a while loop that checks $test <= 15 and increment $test by 1 inside the loop.
+    while ($test <= 15) {
+        //Before incrementing $test, output each iteration (echo $test with newline) and display results.
+        echo $test . PHP_EOL;
+        $test++;
+        usleep(50000);
+    }
 
 
 ?>
