9097d51e4972bd592210a2c47473b8504c7a50d2
Flysociety <Flysociety@Flysocietys-MacBook-Pro.local>
2014-09-17 17:39:13 -0500

    Indentation converted to spaces.

--- for.php
+++ for.php
@@ -1,25 +1,24 @@
 <?php
 
+        //output and get starting number
+        fwrite(STDOUT, "please enter starting number: ");   
+            //user enters starting number (trim to fix "enter" in keyboard for new line)
+            $startNum = trim(fgets(STDIN));                 
+        //output and get ending number
+        fwrite(STDOUT,"please enter ending number: ");      
+            //user enters ending number (trim to fix "enter")
+            $endNum = trim(fgets(STDIN));                   
+        //output and and ask user to ask for amount to count by
+        fwrite(STDOUT,"choose an amount to count by: ");    
+            $counter = trim(fgets(STDIN));
 
 
-
-fwrite(STDOUT, "please enter starting number: ");	//output and get starting number
-	$startNum = trim(fgets(STDIN));					//user enters starting number (trim to fix "enter" in keyboard for new line)
-
-fwrite(STDOUT,"please enter ending number: ");		//output and get ending number
-	$endNum = trim(fgets(STDIN));					//user enters ending number (trim to fix "enter")
-
-fwrite(STDOUT,"choose an amount to count by: ");	//output and and ask user to ask for amount to count by
-	$counter = trim(fgets(STDIN));
-
-
-
-		for ($i=$startNum; $i <= $endNum; $i*=$counter) {	//FOR loop; starting variable; while variable is less than end number; incrementor by amount entered by user
-
-		echo "$i\n";										//output variable
-		usleep(500000);}									//pause
-
-
+        //FOR loop; starting variable; while variable is less than end number; incrementor by amount entered by user
+        for ($i=$startNum; $i <= $endNum; $i*=$counter) {   
+        //output variable
+        echo "$i\n";
+        //pause                                     
+        usleep(500000);}                                    
 
 ?>
 
