e1334c5b7f8502c66a4edd76d53f96c6b0dd30c9
Flysociety <Flysociety@Flysocietys-MacBook-Pro.local>
2014-09-17 18:06:54 -0500

    Formatting applied.

--- if_and_or.php
+++ if_and_or.php
@@ -1,40 +1,40 @@
 <?php
 
-$x = 0;
-$y = 5;
-$z = 10;
+    $x = 0;
+    $y = 5;
+    $z = 10;
 
-// if $x < $y < $z then echo "{$x} < {$y} < {$z}\n";
-	if ($x < $y && $y <$z){
-		echo "$x < $y < $z ".PHP_EOL;
-	}
+        // if $x < $y < $z then echo "{$x} < {$y} < {$z}\n";
+        if ($x < $y && $y < $z) {
+            echo "$x < $y < $z ".PHP_EOL;
+        }
 
-// if $x is greater than 0 OR less than 10, echo the result as a sentence "$x is greater than 0 OR less than 10".
-// repeat the if statement for $y and $z.
+        // if $x is greater than 0 OR less than 10, echo the result as a sentence "$x is greater than 0 OR less than 10".
+        // repeat the if statement for $y and $z.
 
-	if ($x > 0 || $x < 10){
-		echo "$x is greater than 0 OR less than 10".PHP_EOL;
-	}
-	if ($y > 0 || $y < 10){
-		echo "$y is greater than 0 OR less than 10".PHP_EOL;
-	}
-	if ($z > 0 || $z < 10){
-		echo "$z is greater than 0 OR less than 10".PHP_EOL;
-	}
+        if ($x > 0 || $x < 10) {
+            echo "$x is greater than 0 OR less than 10".PHP_EOL;
+        }
+        if ($y > 0 || $y < 10) {
+            echo "$y is greater than 0 OR less than 10".PHP_EOL;
+        }
+        if ($z > 0 || $z < 10) {
+            echo "$z is greater than 0 OR less than 10".PHP_EOL;
+        }
 
 
-// If $x is greater than 0 AND less than 10, echo the result as a sentence "$x is greater than 0 AND less than 10".
-// repeat the if statement for $y and $z.
+        // If $x is greater than 0 AND less than 10, echo the result as a sentence "$x is greater than 0 AND less than 10".
+        // repeat the if statement for $y and $z.
 
-	if ($x > 0 && $x < 10){
-		echo "$x is greater than AND less than 10".PHP_EOL;
-	}
-	if ($y > 0 && $y < 10){
-		echo "$y is greater than AND less than 10".PHP_EOL;
-	}
-	if ($z > 0 && $z < 10){
-		echo "$z is greater than AND less than 10".PHP_EOL;
-	}
+        if ($x > 0 && $x < 10) {
+            echo "$x is greater than AND less than 10".PHP_EOL;
+        }
+        if ($y > 0 && $y < 10) {
+            echo "$y is greater than AND less than 10".PHP_EOL;
+        }
+        if ($z > 0 && $z < 10) {
+            echo "$z is greater than AND less than 10".PHP_EOL;
+        }
 
 ?>
 
