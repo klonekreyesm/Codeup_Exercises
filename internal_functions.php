cf8b0560371098697dc4b2e3f40687c936ba7053
Flysociety <Flysociety@Flysocietys-MacBook-Pro.local>
2014-09-17 18:14:43 -0500

    Formatting applied.

--- internal_functions.php
+++ internal_functions.php
@@ -1,49 +1,29 @@
 <?php
 
-	
-	$nothing = NULL;
-	$something = '';
-	$array = array(1,2,3);
 
-function check($variable){
-		//are you a set of data?
-		if (isset($variable)){	
-		return "The variable is SET".PHP_EOL; 
-		}	
-		//are you empty?
-		elseif (empty($variable)){					
-		return "The variable is empty".PHP_EOL;
-		}
-}
-
-	
-	echo check($nothing);
-	echo check($something);
-	echo check($array);
-
-	$serialize_array = serialize($array);
-	var_dump ($serialize_array);
-	var_dump ($array);
-	
+    $nothing = NULL;
+    $something = '';
+    $array = array(1,2,3);
 
 
+        function check($variable) {
+            //are you a set of data?
+            if (isset($variable)) {  
+                return "The variable is SET".PHP_EOL; 
+            }   //are you empty?
+            elseif (empty($variable)) {                  
+                return "The variable is empty".PHP_EOL;
+            }
+        }
 
+    
+        echo check($nothing);
+        echo check($something);
+        echo check($array);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        $serialize_array = serialize($array);
+        var_dump ($serialize_array);
+        var_dump ($array);
+    
 
 ?>
