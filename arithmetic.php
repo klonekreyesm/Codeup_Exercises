cb1b9c458db3da86d8249ea5a5cc7e774285612d
Flysociety <Flysociety@Flysocietys-MacBook-Pro.local>
2014-09-17 17:27:53 -0500

    Formatting applied - indented using spaces, converted.

--- arithmetic.php
+++ arithmetic.php
@@ -1,88 +1,76 @@
 <?php
 
-        //define addition function
-        function add($a,$b){	
-			if (is_numeric($a) && is_numeric($b)) {					
-				return $a + $b. PHP_EOL;
-				usleep (500000);
-			}
-			else {
-        		echo "ERROR: Both arguments must be of numberic value".PHP_EOL;
-    		}
-		}
-		//define subtraction function
-		function subtract($a,$b){									
-			if (is_numeric($a) && is_numeric($b)){									
-				return $a - $b. PHP_EOL;
-				usleep (500000);
-			}
-			else{
-        		echo "ERROR: Both arguments must be of numeric value".PHP_EOL;
-    		}
-		}
-		//define multiplication function
-		function multiply($a,$b){									
-			if(is_numeric($a) && is_numeric($b)){
-				return $a * $b. PHP_EOL;
-				usleep (500000);
-			}
-			else{
-				echo "ERROR: Both arguments must be of numeric value".PHP_EOL;
-			}
-		}
-		//define division function
-		function divide ($a,$b){									
-			if(is_numeric($a) && is_numeric($b)){
-				if($b == 0){
-					echo "ERROR: FALSE".PHP_EOL;
-				}
-				else{
-					return $a / $b. PHP_EOL;
-					usleep (500000);
-				}
-			}
-			else{
-				echo "ERROR: Both arguments must be of numeric value".PHP_EOL;
-			}
-		}
-		//define modulus function
-	 	function remainder($a,$b){									
-	 		if (is_numeric($a) && is_numeric($b)){
-	 			if($b == 0){
-					echo "ERROR: FALSE".PHP_EOL;			
-				}
-				else{
-					return $a % $b. PHP_EOL;
-					usleep (500000);
-				}
-			}
-			else{
-				echo "ERROR: Both arguments must be of numeric value".PHP_EOL;
-			}
-		}
-	//test code by executing 
-	echo add(0,1);												
-	echo subtract(3,1);					
-	echo $add_and_then_subtract;
+    //define addition function
+    function add($a,$b){    
+        if (is_numeric($a) && is_numeric($b)) {                 
+            return $a + $b. PHP_EOL;
+            usleep (500000);
+            }
+        else { echo "ERROR: Both arguments must be of numberic value".PHP_EOL;
+            }
+        }
+        //define subtraction function
+    function subtract($a,$b){                                   
+        if (is_numeric($a) && is_numeric($b)) {                                  
+            return $a - $b. PHP_EOL;
+            usleep (500000);
+            }
+        else { echo "ERROR: Both arguments must be of numeric value".PHP_EOL;
+            }
+        }
+        //define multiplication function
+        function multiply($a,$b){                                   
+            if (is_numeric($a) && is_numeric($b)) {
+                return $a * $b. PHP_EOL;
+                usleep (500000);
+            }
+            else{ echo "ERROR: Both arguments must be of numeric value".PHP_EOL;
+            }
+        }
+        //define division function
+        function divide ($a,$b){                                    
+            if (is_numeric($a) && is_numeric($b)) {
+                if($b == 0){
+                    echo "ERROR: FALSE".PHP_EOL;
+                }
+                else{return $a / $b. PHP_EOL;
+                     usleep (500000);
+                }
+            }
+            else{ echo "ERROR: Both arguments must be of numeric value".PHP_EOL;
+            }
+        }
+        //define modulus function
+        function remainder($a,$b){                                  
+            if (is_numeric($a) && is_numeric($b)) {
+                if($b == 0){
+                    echo "ERROR: FALSE".PHP_EOL;            
+                }
+                else{ return $a % $b. PHP_EOL;
+                      usleep (500000);
+                }
+            }
+            else{ echo "ERROR: Both arguments must be of numeric value".PHP_EOL;
+            }
+        }
+    
+    //Below are examples of code to test the above functions: 
+
+            echo add(0,1);                                              
+            echo subtract(3,1);                 
+            echo $add_and_then_subtract;
 
 
 
-	echo multiply(1,3);					
-	echo divide(8,2);
-	echo remainder(40,3);
-	echo add('Low',0);
-	echo subtract ('Low','test');
-	echo remainder (12,'test');
-	echo divide(12,0);
-	echo remainder (3,0);
+            echo multiply(1,3);                 
+            echo divide(8,2);
+            echo remainder(40,3);
+            echo add('Low',0);
+            echo subtract ('Low','test');
+            echo remainder (12,'test');
+            echo divide(12,0);
+            echo remainder (3,0);
 
-	
-
-
-
-
-
-	
 
 ?>
 
