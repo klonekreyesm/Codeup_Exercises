efe9c6fac75fa8b24dcc27e5bda52d0a33297b50
Flysociety <Flysociety@Flysocietys-MacBook-Pro.local>
2014-09-17 17:30:57 -0500

    final commit - formatting applied

--- break_continue.php
+++ break_continue.php
@@ -1,17 +1,13 @@
 <?php
 
-//for loop that counts to 100 / break at 10
-echo $i . PHP_EOL;	
+    //for loop that counts to 100 / breaks at 10
+    echo $i . PHP_EOL;  
 
-	for ($i = 1; $i<=100; $i++){						
-		
-		if ($i == 10){									
-		 break;											
-														
-		}
-	}
-
-
+    for ($i = 1; $i<=100; $i++) {                       
+        if ($i == 10) {                                 
+        break;                                          
+        }
+    }
 
 
 ?>
