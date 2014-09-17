f6a34378ff2c9083b2e8f31ad1de9d5cb476e461
Flysociety <Flysociety@Flysocietys-MacBook-Pro.local>
2014-09-17 18:04:20 -0500

    Converted indentation to spaces, formatting.

--- if.php
+++ if.php
@@ -1,36 +1,36 @@
 <?php
-// if / else statement in PHP outputing appropriate responses based on variables given
-	$a=10;
-	$b = 3;
-	$c = '1';
+    // if / else statement in PHP outputing appropriate responses based on variables given
+
+    $a=10;
+    $b = 3;
+    $c = '1';
 
 
-		if ($a < $b) {
-		    echo "$a is less than $b \n";
-		}
-		else {
-		    echo "$a is greater than $b \n";
-		}
-		if ($b >= $c) {
-		    echo "$b is greater than or equal to $c \n";
-		}
-		else{ 
-		    echo "$b is less than $c \n";
-		}
+    if ($a < $b) {
+        echo "$a is less than $b \n";
+    }else {
+            echo "$a is greater than $b \n";
+    }
+    
+    if ($b >= $c) {
+        echo "$b is greater than or equal to $c \n";
+    }else { 
+            echo "$b is less than $c \n";
+    }
 
-// combine the next 4 conditionals into one if/else/elseif block that checks in order by: identical, equal, not identical, not equal
+    // combine the next 4 conditionals into one if/else/elseif block that checks in order by: identical, equal, not identical, not equal
 
-		if ($b == $c) {
-	   		echo "$b is equal to $c \n";
-		} 
-	  	elseif ($b === $c) {
-	    	echo "$b is identical to $c \n";
-		}
-		elseif ($b != $c) {
-	    	echo "$b is not equal to $c \n";
-		} 
-		elseif($b !== $c) {
-	    	echo "$b is not identical to $c \n";
-		}
+    if ($b == $c) {
+        echo "$b is equal to $c \n";
+    } 
+    elseif ($b === $c) {
+        echo "$b is identical to $c \n";
+    }
+    elseif ($b != $c) {
+        echo "$b is not equal to $c \n";
+    } 
+    elseif($b !== $c) {
+        echo "$b is not identical to $c \n";
+    }
 
 ?>
