39bc6f00f0473f12752fffc76d94ec758fee7984
Flysociety <Flysociety@Flysocietys-MacBook-Pro.local>
2014-09-17 18:12:34 -0500

    Converted indentation to spaces.

--- if_homework.php
+++ if_homework.php
@@ -1,10 +1,10 @@
 <?php
 
-	$hungry = FALSE;
-	$food_available = TRUE;
+    $hungry = FALSE;
+    $food_available = TRUE;
 
-	if ( $hungry == FALSE && $food_available == TRUE ) {
-		echo "We are fed and good to go.";
-	}
+    if ( $hungry == FALSE && $food_available == TRUE ) {
+        echo "We are fed and good to go.";
+    }
 
 ?>
